9f6b04d15bdf905af972fc36ddc6bac6ecc0518b
Glitch (quilt-hyena) <none>
2018-05-05 18:30:30 +0000

    💪😄 Updated with Glitch

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+//probot can create comment on new issue opened
+//https://github.com/yamarane/test-probot-repo2/issues/5
 module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  robot.on('issues.opened', (context) => {
+    const body = 'Hello world from Probot';
+    context.github.issues.createComment(context.issue({ body }));
+  });
 }
+
